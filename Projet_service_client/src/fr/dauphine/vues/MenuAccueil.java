db8d2a40bfeeabf9192b68781b90fcc6d8b2edd4
utilisateur <utilisateur@Florian-PC.home>
2015-04-06 00:23:25 +0200

    deco + observer dans la bibliotheque

--- Projet_service_client/src/fr/dauphine/vues/MenuAccueil.java
+++ Projet_service_client/src/fr/dauphine/vues/MenuAccueil.java
@@ -19,6 +19,8 @@ import fr.dauphine.widgets.JInternalFrameMonProfil;
 
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
+import javax.swing.JButton;
+import javax.swing.LayoutStyle.ComponentPlacement;
 
 
 public class MenuAccueil extends JMenuBar {
@@ -26,6 +28,7 @@ public class MenuAccueil extends JMenuBar {
 	private static final long serialVersionUID = 1L;
 	private ActionListener ActionListenerGestionBO;
 	private ActionListener ActionListenerMonProfil;
+	private ActionListener actionDeconnection;
 
 	private JMenu menuLivre = new JMenu("Livre");
 	private JMenuItem menuVoirLivre = new JMenuItem("Voir Livres");
@@ -59,17 +62,25 @@ public class MenuAccueil extends JMenuBar {
 		lbl_personne.setHorizontalAlignment(SwingConstants.RIGHT);
 		lbl_personne.setText(Session.getIntitule());
 		lbl_personne.setVerticalAlignment(JLabel.CENTER);
+		
+		JButton bt_deco = new JButton("Se deconnecter");
+		bt_deco.addActionListener(actionDeconnection);
+		
 		GroupLayout gl_panel = new GroupLayout(panel);
 		gl_panel.setHorizontalGroup(
 			gl_panel.createParallelGroup(Alignment.TRAILING)
 				.addGroup(gl_panel.createSequentialGroup()
 					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
 					.addComponent(lbl_personne, GroupLayout.PREFERRED_SIZE, 280, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(bt_deco)
 					.addContainerGap())
 		);
 		gl_panel.setVerticalGroup(
 			gl_panel.createParallelGroup(Alignment.LEADING)
-				.addComponent(lbl_personne, GroupLayout.DEFAULT_SIZE, 25, Short.MAX_VALUE)
+				.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
+					.addComponent(lbl_personne, GroupLayout.DEFAULT_SIZE, 25, Short.MAX_VALUE)
+					.addComponent(bt_deco))
 		);
 		panel.setLayout(gl_panel);
 		
@@ -103,15 +114,14 @@ public class MenuAccueil extends JMenuBar {
 
 			}
 		};
+		
+		actionDeconnection = new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				Session.seDeconnecter();
+				((Accueil)getParent().getParent().getParent()).seDeconnecter();
+			}
+		}; 
 
 	}
-
-
-
-
-
-
-
-
-
 }
